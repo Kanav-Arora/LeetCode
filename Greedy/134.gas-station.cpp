b9b50b0f288e111bdf872df7393faac421af6869
Kanav Arora <kanavarora1515@gmail.com>
2022-10-06 18:29:42 +0530

    134. file added

---
+++ Greedy/134.gas-station.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=134 lang=cpp
+ *
+ * [134] Gas Station
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        int n = gas.size();
+        int total(0), subsum(INT_MAX), start(0);
+        for (int i = 0; i < n; ++i)
+        {
+            total += gas[i] - cost[i];
+            if (total < subsum)
+            {
+                subsum = total;
+                start = i + 1;
+            }
+        }
+        return (total < 0) ? -1 : (start % n);
+    }
+};
+// @lc code=end
