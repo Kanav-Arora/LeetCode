9f36655027e5c7a14b24398b94fd5120540e44e5
Kanav Arora <kanavarora1515@gmail.com>
2022-10-07 10:06:04 +0530

    732. file added

---
+++ Hash Map/732.my-calendar-iii.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=732 lang=cpp
+ *
+ * [732] My Calendar III
+ */
+
+// @lc code=start
+class MyCalendarThree {
+private:
+	map<int, int> mp;
+public:
+	MyCalendarThree() {
+
+	}
+
+	int book(int start, int end) {
+		mp[start]++;
+		mp[end]--;
+		int maxBooking = 0, sum = 0;
+		for(auto &x : mp) {
+			sum += x.second;
+			maxBooking = max(maxBooking, sum);
+		}
+		return maxBooking;
+	}
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
+// @lc code=end
+
