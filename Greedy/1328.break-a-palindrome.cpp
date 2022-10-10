257127f421dc42395a0c9d198e710a3f5e8be9b8
Kanav Arora <kanavarora1515@gmail.com>
2022-10-10 21:35:10 +0530

    1328. file added

---
+++ Greedy/1328.break-a-palindrome.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1328 lang=cpp
+ *
+ * [1328] Break a Palindrome
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string breakPalindrome(string palindrome)
+    {
+        int n = palindrome.size();
+        if (n < 2)
+        {
+            return "";
+        }
+        int flag = 0;
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (palindrome[i] != 'a')
+            {
+                flag = 1;
+                palindrome[i] = 'a';
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            palindrome[n - 1] = 'b';
+        }
+        return palindrome;
+    }
+};
+// @lc code=end
