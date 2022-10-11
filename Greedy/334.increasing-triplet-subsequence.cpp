6fdb62a29908d2a769c5e695d2793ee9f9329a53
Kanav Arora <kanavarora1515@gmail.com>
2022-10-11 16:26:30 +0530

    334. file added

---
+++ Greedy/334.increasing-triplet-subsequence.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=334 lang=cpp
+ *
+ * [334] Increasing Triplet Subsequence
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool increasingTriplet(vector<int> &nums)
+    {
+        int n = nums.size();
+        int first = INT_MAX;
+        int second = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] <= first)
+            {
+                first = nums[i];
+            }
+            else if (nums[i] <= second)
+            {
+                second = nums[i];
+            }
+            else
+                return true;
+        }
+
+        return false;
+    }
+};
+// @lc code=end
