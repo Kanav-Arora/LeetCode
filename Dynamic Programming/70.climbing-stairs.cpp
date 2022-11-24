9926ba73b18e252dfb3f0b32b38c180e1477485d
Kanav Arora <kanavarora1515@gmail.com>
2022-11-24 11:02:11 +0530

    70. files added

---
+++ Dynamic Programming/70.climbing-stairs.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+        int prev = 2, prev2 = 1, res;
+        for (int i = 3; i <= n; i++) {
+            res = prev + prev2;
+            prev2 = prev;
+            prev = res;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
