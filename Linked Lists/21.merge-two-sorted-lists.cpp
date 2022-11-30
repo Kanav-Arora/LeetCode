444993357e4c1a3ca54df5fce0c5f669e28f033a
Kanav Arora <kanavarora1515@gmail.com>
2022-11-30 21:22:05 +0530

    21. file added

---
+++ Linked Lists/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!l1)  return l2;
+        if(!l2)  return l1;
+        
+        if(l1 -> val <= l2 -> val)
+        {
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+        
+		else
+        {
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+    }
+};
+// @lc code=end
+
