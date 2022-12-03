a6340831bb1ff6e4baaeb20663c9d15b651286cb
Kanav Arora <kanavarora1515@gmail.com>
2022-12-03 09:31:54 +0530

    451. file added

---
+++ Heap(Priority Queue)/451.sort-characters-by-frequency.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=451 lang=cpp
+ *
+ * [451] Sort Characters By Frequency
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> freq;
+        priority_queue<pair<int,char>> maxheap; 
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            maxheap.push({it.second,it.first}); 
+        s="";   
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second);
+            maxheap.pop();
+        }
+        return s;
+    }
+};
+// @lc code=end
+
