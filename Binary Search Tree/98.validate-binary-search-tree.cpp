d364ffe835aca98583286f120f7f6b6016d64090
Kanav Arora <kanavarora1515@gmail.com>
2022-12-05 15:36:14 +0530

    98. files added

---
+++ Binary Search Tree/98.validate-binary-search-tree.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+
+			return validate(root, std::numeric_limits<long>::min(), std::numeric_limits<long>::max() );
+		}
+
+	private:
+		bool validate(TreeNode* node, long lower, long upper){
+
+			if( node == NULL ){
+
+				return true;
+			}
+
+			if( (lower < node->val) && (node->val < upper) ){
+
+				return validate(node->left, lower, node->val) && validate(node->right, node->val, upper);
+			}
+			else{
+				return false;
+			}
+
+
+		}
+};
+// @lc code=end
+
