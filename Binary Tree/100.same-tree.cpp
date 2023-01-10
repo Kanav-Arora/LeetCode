4415da4cbff7b145328b87fadd861e013d50fd80
Kanav Arora <kanavarora1515@gmail.com>
2023-01-10 13:58:28 +0530

    100. file added

---
+++ Binary Tree/100.same-tree.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=100 lang=cpp
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)  return true;
+        if(p==NULL || q==NULL)  return false;
+
+        bool lt = isSameTree(p->left, q->left);
+        bool rt = isSameTree(p->right,q->right);
+
+        return lt && rt && p->val==q->val;
+    }
+};
+// @lc code=end
+
